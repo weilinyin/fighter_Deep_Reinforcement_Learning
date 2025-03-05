970db51d4e59340769381189c39863428347b6b6
weilinyin <weilinyin@mail.nwpu.edu.cn>
2025-03-05 19:40:04 +0800

    终于改好了

--- no-policy.py
+++ no-policy.py
@@ -5,10 +5,11 @@ import matplotlib.pyplot as plt
 plt.rcParams['font.sans-serif'] = ['SimHei']  # 指定默认字体
 plt.rcParams['axes.unicode_minus'] = False  # 解决保存图像时负号'-'显示为方块的问题
 
-myenv = FighterEnv_nopolicy(True , dt=0.1 ,Dt = 0.1)
+myenv = FighterEnv_nopolicy(True , dt=0.01 ,Dt = 0.01)
 
 
-
+r = np.array(myenv.plotdata["defender"]["r"])
+print(min(r))
 
 
 
