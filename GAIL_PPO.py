35f8dbd50d68854923e6d5eb305c0b442cf68a4d
weilinyin <weilinyin@mail.nwpu.edu.cn>
2025-03-10 17:33:47 +0800

    魔改buffer

--- GAIL_PPO.py
+++ GAIL_PPO.py
@@ -4,19 +4,145 @@ import torch as th
 from math import cos , exp
 from MyEnvs import relative
 import numpy as np
+from MyEnvs import FighterEnv_2D
+from stable_baselines3.common.buffers import RolloutBuffer
+from typing import Optional
+from collections.abc import Generator
+from stable_baselines3.common.type_aliases import (
+    DictReplayBufferSamples,
+    DictRolloutBufferSamples,
+    ReplayBufferSamples,
+    RolloutBufferSamples,
+)
+from stable_baselines3.common.vec_env import VecNormalize
+
+
+
+class CustomRolloutBuffer(RolloutBuffer):
+    #魔改了buffer，增加了专家策略
+    expert_actions:np.ndarray
+    def get(self, batch_size: Optional[int] = None) -> Generator[RolloutBufferSamples, None, None]:
+        assert self.full, ""
+        indices = np.random.permutation(self.buffer_size * self.n_envs)
+        # Prepare the data
+        if not self.generator_ready:
+            _tensor_names = [
+                "observations",
+                "actions",
+                "expert_actions",
+                "values",
+                "log_probs",
+                "advantages",
+                "returns",
+            ]
+
+            for tensor in _tensor_names:
+                self.__dict__[tensor] = self.swap_and_flatten(self.__dict__[tensor])
+            self.generator_ready = True
+
+        # Return everything, don't create minibatches
+        if batch_size is None:
+            batch_size = self.buffer_size * self.n_envs
+
+        start_idx = 0
+        while start_idx < self.buffer_size * self.n_envs:
+            yield self._get_samples(indices[start_idx : start_idx + batch_size])
+            start_idx += batch_size
+    
+    def reset(self) -> None:
+        self.observations = np.zeros((self.buffer_size, self.n_envs, *self.obs_shape), dtype=np.float32)
+        self.actions = np.zeros((self.buffer_size, self.n_envs, self.action_dim), dtype=np.float32)
+        self.rewards = np.zeros((self.buffer_size, self.n_envs), dtype=np.float32)
+        self.returns = np.zeros((self.buffer_size, self.n_envs), dtype=np.float32)
+        self.episode_starts = np.zeros((self.buffer_size, self.n_envs), dtype=np.float32)
+        self.values = np.zeros((self.buffer_size, self.n_envs), dtype=np.float32)
+        self.log_probs = np.zeros((self.buffer_size, self.n_envs), dtype=np.float32)
+        self.advantages = np.zeros((self.buffer_size, self.n_envs), dtype=np.float32)
+        self.expert_actions = np.zeros((self.buffer_size, self.n_envs), dtype=np.float32)
+        self.generator_ready = False
+        super().reset()
+
+    def add(
+        self,
+        obs: np.ndarray,
+        action: np.ndarray,
+        reward: np.ndarray,
+        episode_start: np.ndarray,
+        value: th.Tensor,
+        log_prob: th.Tensor,
+        expert_action: np.ndarray,  # Added parameter for expert action
+    ) -> None:
+        """
+        :param obs: Observation
+        :param action: Action
+        :param reward:
+        :param episode_start: Start of episode signal.
+        :param value: estimated value of the current state
+            following the current policy.
+        :param log_prob: log probability of the action
+            following the current policy.
+        """
+        if len(log_prob.shape) == 0:
+            # Reshape 0-d tensor to avoid error
+            log_prob = log_prob.reshape(-1, 1)
+
+        # Reshape needed when using multiple envs with discrete observations
+        # as numpy cannot broadcast (n_discrete,) to (n_discrete, 1)
+        if isinstance(self.observation_space, spaces.Discrete):
+            obs = obs.reshape((self.n_envs, *self.obs_shape))
+
+        # Reshape to handle multi-dim and discrete action spaces, see GH #970 #1392
+        action = action.reshape((self.n_envs, self.action_dim))
+
+        self.observations[self.pos] = np.array(obs)
+        self.actions[self.pos] = np.array(action)
+        self.rewards[self.pos] = np.array(reward)
+        self.episode_starts[self.pos] = np.array(episode_start)
+        self.values[self.pos] = value.clone().cpu().numpy().flatten()
+        self.log_probs[self.pos] = log_prob.clone().cpu().numpy()
+        self.expert_actions[self.pos] = np.array(expert_action)  # Add expert action to the buffer
+        self.pos += 1
+        if self.pos == self.buffer_size:
+            self.full = True
+    def _get_samples(
+        self,
+        batch_inds: np.ndarray,
+        env: Optional[VecNormalize] = None,
+    ) -> RolloutBufferSamples:
+        data = (
+            self.observations[batch_inds],
+            self.actions[batch_inds],
+            self.expert_actions[batch_inds],  # Include expert actions in the samples
+            self.values[batch_inds].flatten(),
+            self.log_probs[batch_inds].flatten(),
+            self.advantages[batch_inds].flatten(),
+            self.returns[batch_inds].flatten(),
+        )
+        return RolloutBufferSamples(*tuple(map(self.to_torch, data)))
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 
 
 class expert_generator:
-    def __init__(self , FD:relative , FT:relative , t_0 ,c_f):
-
-        self.t_0 = t_0
-        self.FD = FD
-        self.FT = FT
-        self.c_0 = self.FD.dq_z
-        self.t_f = t_0 - FD.r / FD.dr -0.5
+    def __init__(self ,env:FighterEnv_2D ,c_f=-0.002):
+        self.env = env
+        self.t_0 = env.t_0
+        self.c_0 = self.env.FD.dq_z
+        self.t_f = self.t_0 - self.env.FD.r / self.env.FD.dr -0.5
         self.c_f = c_f
 
 
@@ -24,10 +150,10 @@ class expert_generator:
         
         if t < self.t_f:
 
-            k_FD = self.FD.dr / self.FD.r
-            r_FD = self.FD.r
-            q_FD = self.FD.q_z
-            psi_VF = self.FD.target.psi
+            k_FD = self.env.FD.dr / self.env.FD.r
+            r_FD = self.env.FD.r
+            q_FD = self.env.FD.q_z
+            psi_VF = self.env.FD.target.psi
 
             
             
@@ -41,13 +167,21 @@ class expert_generator:
             return a_E
         else:
             
-            self.FT.proportional_navigation()
-            _ , a_E = self.FT.calculate_a()
+            self.env.FT.proportional_navigation()
+            _ , a_E = self.env.FT.calculate_a()
 
             return a_E /(9.81*2)
 
 
+    def sample(self):
+        a = self.generate(self.env.t)
 
+        r_1 , q_y1 , q_z1 = self.env.FD.state("defender")
+        r_2 , q_y2 , q_z2 = self.env.FT.state("fighter")
+        obs = np.array([r_1 , q_y1 , q_z1 , r_2 , q_y2 , q_z2])
+
+        return obs , a
+        
 
         
 
@@ -60,11 +194,11 @@ class GAILDiscriminator(nn.Module):
     def __init__(self, observation_space, action_space):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(observation_space.shape[0] + action_space.shape[0], 64),
-            nn.Tanh(),
-            nn.Linear(64, 32),
-            nn.Tanh(),
-            nn.Linear(32, 1),
+            nn.Linear(observation_space.shape[0] + action_space.shape[0], 200),
+            nn.ReLU(),
+            nn.Linear(200, 200),
+            nn.ReLU(),
+            nn.Linear(200, 1),
             nn.Sigmoid()
         )
     
@@ -72,8 +206,8 @@ class GAILDiscriminator(nn.Module):
         return self.net(th.cat([states, actions], dim=1))
 
 class GAIL_PPO(PPO):
-    def __init__(self, expert_generator, **kwargs):
-        super().__init__(**kwargs)
+    def __init__(self, expert_generator:expert_generator, **kwargs):
+        super().__init__(rollout_buffer_class=CustomRolloutBuffer, **kwargs)
         # 初始化判别器
         self.discriminator = GAILDiscriminator(
             self.observation_space, 
@@ -96,7 +230,7 @@ class GAIL_PPO(PPO):
         agent_obs, agent_acts, _ = self.collect_rollouts()
         
         # 获取专家数据（实时生成）
-        expert_obs, expert_acts = self.expert_generator.sample()
+        expert_obs, expert_acts = self.expert_generator.sample(agent_obs)
         
         # 训练判别器
         for _ in range(5):  # 对抗训练次数
--- expert_test.py
+++ expert_test.py
@@ -8,34 +8,71 @@ plt.rcParams['font.sans-serif'] = ['SimHei']  # 指定默认字体
 plt.rcParams['axes.unicode_minus'] = False  # 解决保存图像时负号'-'显示为方块的问题
 
 
-myenv = FighterEnv_2D(False,Dt = 0.01 ,dt = 0.01)
+myenv = FighterEnv_2D(True,Dt = 0.01 ,dt = 0.01)
 
 
 
 target_c_f = 0
-c_f = 0.1
-while c_f > -0.1:
-    obs , _ = myenv.reset()
-    
-    expert = expert_generator(myenv.FD , myenv.FT , myenv.t_0 , c_f )
-    
-    while not (myenv.success or myenv.fail):
-        a_E = expert.generate(myenv.t)
-        action = np.array([a_E])
-        obs, rewards, dones, _ , _ = myenv.step(action)
-    
-    
-    if rewards >30:
-        target_c_f = c_f
-        print(target_c_f)
-        break
-    
-    c_f -= 0.001
+c_f = -0.002
 
-
-print(target_c_f)
+obs , _ = myenv.reset()
+    
+expert = expert_generator(myenv.FD , myenv.FT , myenv.t_0 , c_f )
+    
+while not (myenv.success or myenv.fail):
+    a_E = expert.generate(myenv.t)
+    action = np.array([a_E])
+    obs, rewards, dones, _ , _ = myenv.step(action)
 
 
 
 
 print(rewards)
+r = myenv.plotdata["defender"]["r"]
+print(min(r))
+
+plt.figure()
+plt.plot(myenv.t_array , myenv.plotdata["defender"]["r"],label = "R_FD")
+plt.plot(myenv.t_array , myenv.plotdata["fighter"]["r"], label = "R_FT")
+plt.title('相对距离图')
+plt.xlabel('t/s')
+plt.ylabel('r/m')
+plt.legend()
+
+
+
+
+
+
+plt.figure()
+plt.plot(myenv.plotdata["defender"]["x"] , myenv.plotdata["defender"]["z"],label = "防御弹")
+plt.plot(myenv.plotdata["fighter"]["x"] , myenv.plotdata["fighter"]["z"], label = "战斗机")
+plt.title('侧向轨迹图')
+plt.xlabel('x/m')
+plt.ylabel('z/m')
+plt.legend()
+
+
+
+plt.figure()
+plt.plot(myenv.t_array , myenv.plotdata["defender"]["psi"],label = "防御弹")
+plt.plot(myenv.t_array , myenv.plotdata["fighter"]["psi"], label = "战斗机")
+plt.title('弹道偏角图')
+plt.xlabel('t/s')
+plt.ylabel('psi/rad')
+plt.legend()
+
+
+
+plt.figure()
+plt.plot(myenv.t_array , myenv.plotdata["defender"]["a_z"],label = "防御弹")
+plt.plot(myenv.t_array , myenv.plotdata["fighter"]["a_z"], label = "战斗机")
+plt.title('侧向加速度图')
+plt.xlabel('t/s')
+plt.ylabel('a_z/(m s^-2)')
+plt.legend()
+
+
+
+
+plt.show()
